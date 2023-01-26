41240a7a062b3532bc44792016874581547106ef
Brian Brown <brian@thekompanee.com>
2023-01-26 10:11:38 -0600

    construct ready function and add event listeners for cards and overlay

--- src/index.js
+++ src/index.js
@@ -4,7 +4,34 @@ If DOM is still loading call "ready" function when DOM is loaded
 If not then page is loaded and call "ready" function
 */
 if (document.readyState == 'loading') {
-  document.addEventListener('DOMContentLoaded', ready);
+    document.addEventListener('DOMContentLoaded', ready);
 } else {
-  ready();
+    ready();
+}
+
+// "ready" function
+function ready() {
+    // create and array for overlay elements
+    let overlays = Array.from(document.getElementsByClassName('overlay-text'));
+    // create and array for card elements
+    let cards = Array.from(document.getElementsByClassName('card'));
+
+  
+    //for each item in the overlay call event listener click function
+    overlays.forEach(overlay => {
+        overlay.addEventListener('click', () => {
+            // remove visible class
+            overlay.classList.remove('visible');
+            // call start the game function
+            // game.startGame();
+        });
+    });
+
+    //for each card call event listener click function
+    cards.forEach(card => {
+        card.addEventListener('click', () => {
+            // On click function will flip the card over
+            game.flipCard(card);
+        });
+    });
 }
